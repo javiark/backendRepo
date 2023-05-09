5b4673fde64e0f428fb8c32aeb4689a67534c160
Javier <javier778@hotmail.com>
2023-05-09 17:14:34 -0300

    avance

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,8 +1,23 @@
+const Product=require("./../schemas/product.schema")
 const getAllProducts = (req,res)=>{
-    
-    res.status(200).send(`Productos solicitados correctamente`);
+
+    Product.find().then(function(productos){ //Find me devuelve los productos, busca de la coleccion productos, una vez que logre leerlos vamos a ejecutar una funcion. La funcion me devuelve los productos que hay obtenido
+        res.status(200).send({
+            msg: `Productos obtenidos correctamente`,
+            productos: productos // devuelvo los productos
+        });
+    })
+
+
 }
 
+function deleteProduct(req, res){
+    res.status(200).send("Producto borrado correctamente!!!")
+
+}
+
+
 module.exports={
-    getAllProducts
+    getAllProducts, // igual a getAllProduct : getAllProduct
+    deleteProduct
 }
---
+++ schemas/product.schema.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const proudctSchema = new Schema({
+    name: String,
+    price: Number,
+    description: String
+}) //propiedades que quiero q se guarden. Si sobran cosas las va a quitar
+
+module.exports = mongoouse.model("Product", proudctSchema) // nombre del modelo "products" ( se cambia a minuscula y con s ). Se hace una tabla
