11681f7240f43fc5c0a9f5072f9a04b79e97a0a9
JavierMaldonado <javier778@hotmail.com>
2023-05-08 23:23:50 -0300

    funcionando servidor

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hola node")
+const express = require("express");
+
+const app = express();
+const port = 4000;
+
+app.listen(port, ()=>{
+    console.log(`servidor funcionando en puerto ${port}`)
+})
