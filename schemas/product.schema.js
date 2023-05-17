9fbf40888811bd1be79b05b22a647de1ac6e65b9
JavierMaldonado <javier778@hotmail.com>
2023-05-17 07:27:41 -0300

    categorias2

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -12,13 +12,15 @@ const proudctSchema = new Schema({
     description: {type:String, required:true},
     detail:{type:String, required:true},
     price: {type: Number, required:true},
-    image:{type:String, required:true},
+    image:[
+        {type:String, required:true}
+    ] ,
     stock:{type:Number, required:true, default:1},
     active:{type:Boolean, default:true, required:true},
     category:{
         type: mongoose.Schema.Types.ObjectId,
         ref:"Category",
-        // required:true,
+        required:true,
     },
     createAt:{type:Number, default:Date.now()},
     
