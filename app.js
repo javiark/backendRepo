87d36f0ff171aafcb4e33c74cc81c4a067893c03
Javier <javier778@hotmail.com>
2023-05-18 19:27:12 -0300

    juntar todo

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require(`express`);
 const app = express();
+//Rutas de las vistas
+const viewsRoutes=require("./routes/views.routes")
 const productRoutes=require("./routes/product.routes")
 const userRoutes = require("./routes/user.routes")
 const orderRoutes=require("./routes/orders.routes")
@@ -7,6 +9,9 @@ const categoryRoutes=require("./routes/category.routes")
 const upload_routes = require('./routes/upload.routes');
 const cors = require ("cors");
 
+//Cargar configuracion de plantillas de Javascript
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 //middlewares
 app.use(express.json());
 // app.use(express.urlencoded({ extended:true}))
@@ -20,6 +25,8 @@ app.use(cors());
 //     })
 // });
 
+//que las vistas no llamen a api
+app.use(viewsRoutes)
 
 //Definir rutas a usar por mi app express
 app.use("/api",[
---
+++ routes/views.routes.js
@@ -0,0 +1,19 @@
+// DEFINIR LAS RUTAS DE NUESTRAS VISTAS. EJ: PAGES, ROUTES. RUTAS QUE CARGAN UNA VISTA
+
+const express = require("express")
+const router = express.Router();
+const viewsController = require("../controllers/views.controllers")
+
+// La Definicion de todas las rutas y vistas y sus respectivos controladores
+
+//ruta index
+router.get("/", (req, res) =>
+    res.render("index", {  title: "Index Template EJS" })) // devuelve la pagina index. Le puedo mandar variables
+
+router.get("/contact", (req, res) => {
+
+    return res.render("contact")
+
+}); // devuelve la pagina index
+
+module.exports = router;
