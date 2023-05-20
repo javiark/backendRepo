98ff4bc3e6c7b4f6c35fe8420080b28b78ab735f
Javier <javier778@hotmail.com>
2023-05-20 00:25:53 -0300

    views creadas

--- routes/views.routes.js
+++ routes/views.routes.js
@@ -11,10 +11,41 @@ router.get("/", (req, res) =>
     res.render("index", {  title: "Index Template EJS" })) // devuelve la pagina index. Le puedo mandar variables
 
 router.get("/contact", (req, res) => {
-
     return res.render("contact")
+}); 
 
-}); // devuelve la pagina index
+router.get("/location", (req, res) => {
+    return res.render("location")
+}); 
+
+router.get("/order", (req, res) => {
+    return res.render("order")
+}); 
+
+router.get("/product-detail", (req, res) => {
+    return res.render("product-detail")
+});
+
+router.get("/register", (req, res) => {
+    return res.render("register")
+}); 
+
+router.get("/about-us", (req, res) => {
+    return res.render("about-us")
+}); 
+
+router.get("/admin-product", (req, res) => {
+    return res.render("admin-product")
+}); 
+
+router.get("/admin-user", (req, res) => {
+    return res.render("admin-user")
+}); 
+
+router.get("/login", (req, res) => {
+    return res.render("login")
+}); 
+
 
 // para chequear si esta logueada o no la persona. Proteguemos la ruta
 // router.get("/admin-product", (req, res)={return res.render("admi-product")})
