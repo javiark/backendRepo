7ea959d6745bd86c18f0f0e76953f01dc927e19f
JavierMaldonado <javier778@hotmail.com>
2023-05-26 02:18:26 -0300

    imagen arreglado

--- public/js/adminProducts.js
+++ public/js/adminProducts.js
@@ -351,3 +351,11 @@ function editProduct(idx) {
     // console.log("product:", product)
     editIndex = idx;
 }
+
+const actualBtn = document.getElementById('actual-btn');
+
+const fileChosen = document.getElementById('file-chosen');
+
+actualBtn.addEventListener('change', function(){
+  fileChosen.textContent = this.files[0].name
+})
