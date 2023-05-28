b583a9bf909505c36df2851e71ec57009649c64e
JavierMaldonado <javier778@hotmail.com>
2023-05-28 07:24:46 -0300

    anda boton buscar

--- public/js/filter.js
+++ public/js/filter.js
@@ -27,3 +27,40 @@ function metodoFilter(evt){
     }
     renderizarProductos(productosFiltrado)
 };
+
+//-------------BUSCARLO POR BOTON----------------//
+
+function searchProduct(evt){
+    if(evt.keyCode !==13 && evt.target.id !== 'productId'){
+        return
+    }
+    const foundProduct=document.getElementById('searchProductBtn').value.toLowerCase()
+    let resultFind = productos.filter((el)=>{
+        return el.name.toLowerCase().includes(foundProduct)       
+    })
+    if (resultFind.length>0){
+        document.getElementById("productsCount").innerHTML=(`Se encontraron ${resultFind.length} productos`);
+    }else{
+        document.getElementById("productsCount").innerHTML=(`No se han encontrado productos`)
+    }
+
+    renderizarProductos(resultFind)
+}
+
+
+
+function handleBuscarIndex(evt){
+    if(evt.keyCode !==13 && evt.target.id !== 'search-index'){
+        return
+    }
+    const searchValue=document.getElementById('search-product-index').value.toLowerCase()
+    let searchResults = products.filter((el)=>{
+        return el.name.toLowerCase().includes(searchValue) ||
+        el.category.toLowerCase().includes(searchValue)         
+    })
+    if(searchResults.length > 1) {document.querySelector('.section-cards__products-count').innerHTML = `Se encontraron ${searchResults.length} productos`}
+    else{ document.querySelector('.section-cards__products-count').innerHTML = `Se encontró 1 producto`}
+    if(searchResults.length === 0) {document.querySelector('.section-cards__products-count').innerHTML = `No se encontraron productos. Puede buscar con palabras similares.`}
+
+    renderizarProductos(searchResults)
+}
